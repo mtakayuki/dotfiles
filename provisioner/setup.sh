f85b61e8b557a4a3c0013ca01ad7c3f6827c6816
Takayuki Maeda <maeda.takayuki@jp.fujitsu.com>
2017-01-27 16:36:22 +0900

    Recognize cygwin environment

--- provisioner/setup.sh
+++ provisioner/setup.sh
@@ -2,11 +2,25 @@
 
 set -eu
 
-for s in "$(dirname $0)"/scripts/*.sh
-do
-  bash "$s"
-done 
+ansible='ansible-playbook'
+if [ "$(uname -s)" == "Darwin" ]; then
+  os='mac'
+  ansible='/usr/local/bin/ansible-playbook'
+elif [ "$(expr substr $(uname -s) 1 9)" == "CYGWIN_NT" ]; then
+  os='cygwin'
+else
+  os='other'
+fi
+
+if [ -d "$(dirname $0)/scripts/$os" ]; then
+  for s in "$(dirname $0)"/scripts/$os/*.sh
+  do
+    bash "$s"
+  done
+
+fi
 
 cd "$(dirname $0)"/playbooks
-
-/usr/local/bin/ansible-playbook -i hosts site.yml
+if type ${ansible} > /dev/null 2>&1; then
+  /usr/local/bin/ansible-playbook -i hosts site.yml
+fi
